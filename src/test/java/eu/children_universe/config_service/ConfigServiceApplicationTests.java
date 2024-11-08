997bedbac62e5cdec0404f9df41ac668d5acd2a5
Petre Vane <petre.vane@gmail.com>
2024-11-08 11:50:37 +0100

    Initial commit

---
+++ src/test/java/eu/children_universe/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.children_universe.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
